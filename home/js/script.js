2574a16ee49a0b2ea18e741cfebe8e2fb88c55e4
K1NGAARON <gerardaaron1997&gmail.com>
2023-02-10 22:51:50 +0100

    changes to homepage script

--- home/js/script.js
+++ home/js/script.js
@@ -7,14 +7,48 @@ const socialProofContent = [
 // Services
 const servicesTarget = document.querySelector('#servicesHolder');
 const servicesContent = [
-
+    {
+        title: "Service 1",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        img: '/home/assets/img/service1.jpg',
+        url: '',
+    },
+    {
+        title: "Service 2",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        img: '/home/assets/img/service2.jpg',
+        url: '',
+    },
+    {
+        title: "Service 3",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        img: '/home/assets/img/service3.jpg',
+        url: '',
+    },
 ];
 
 
 // Highlighted content
 const highlightedContentTarget = document.querySelector('#highlightedContentHolder');
 const highlightedContent = [
-
+    {
+        title: "This is the first blog",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam quia iure corporis autem ab rem id voluptatem, ipsam quibusdam eum?",
+        img: '/home/assets/img/banner.jpg',
+        url: '',
+    },
+    {
+        title: "This is the second blog",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        img: '/home/assets/img/banner.jpg',
+        url: '',
+    },
+    {
+        title: "This is the third blog",
+        body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam quia iure corporis autem ab rem id voluptatem, ipsam quibusdam eum?",
+        img: '/home/assets/img/banner.jpg',
+        url: '',
+    },
 ];
 
 function createSocialProof(e) {
@@ -22,13 +56,43 @@ function createSocialProof(e) {
 };
 
 function createServices(e) {
+    for (let i = 0; i < servicesContent.length; i++) {
+        let template = `
+            <div class="container">
+                <img src="${servicesContent[i].img}" alt="${servicesContent[i].title}">
+                <div class="content">
+                    <h5>
+                        ${servicesContent[i].title}
+                    </h5>
+                    <p>
+                        ${servicesContent[i].body}
+                    </p>
+                    <a class="btn ghost-btn" href="${servicesContent[i].url}">
+                        Read more
+                    </a>
+                </div>
+            <div>
+        `;
 
+        servicesTarget.insertAdjacentHTML('beforeend', template);
+    }
 };
 
 function createCards() {
     for (let i = 0; i < highlightedContent.length; i++) {
         let template = `
-
+            <div class="card">
+                <img src="${highlightedContent[i].img}" alt="${highlightedContent[i].title}">
+                <h5>
+                    ${highlightedContent[i].title}
+                </h5>
+                <p>
+                    ${highlightedContent[i].body}
+                </p>
+                <a class="btn ghost-btn" href="${highlightedContent[i].url}">
+                    Read more
+                </a>
+            </div>
         `;
 
         highlightedContentTarget.insertAdjacentHTML('beforeend', template);
@@ -36,7 +100,6 @@ function createCards() {
 };
 
 $(document).ready(function() {
-    console.log('1');
-    console.log('2');
-    console.log('3');
+    createServices()
+    createCards();
 });
