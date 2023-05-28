a447e4b7b9d30ba7d0907824c0b7334473ff9b6c
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-28 13:52:22 +0200

    cookie set p

--- js/app.js
+++ js/app.js
@@ -13,3 +13,27 @@ $(document).scroll(function() {
 $("#menu-toggle").click(function() {
     $(".small-menu-wrapper").toggle("active");
 });
+
+const cookieBanner = document.querySelector(".cookie-banner-wrapper");
+const cookiesAccepted = document.querySelector("#accept-cookies");
+const blackOverlay = document.querySelector("#overlay");
+
+// Accept cookies
+cookiesAccepted.addEventListener("click", function acceptCookies() {
+    blackOverlay.classList.add("hide-cookie-banner");
+    cookieBanner.classList.add("hide-cookie-banner");
+    storeCookieConsent();
+});
+
+function storeCookieConsent() {
+    localStorage.cookies = "accepted";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    if ("cookies" in localStorage) {
+        blackOverlay.classList.add("hide-cookie-banner");
+        cookieBanner.classList.add("hide-cookie-banner");
+    } else {
+        // Shows cookieBanner
+    }
+});
